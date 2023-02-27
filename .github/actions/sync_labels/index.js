3dc4e4204d03b4b470f2641696a8136883bec8be
Will Da Silva <will@willdasilva.xyz>
2023-02-27 16:19:13 -0500

    Add `.catch(...)`

--- .github/actions/sync_labels/index.js
+++ .github/actions/sync_labels/index.js
@@ -8,8 +8,9 @@ const config = yaml.load(fs.readFileSync(core.getInput('config'), 'utf8'));
 const octokit = github.getOctokit(core.getInput('token'));
 
 for (const org of config['orgs']) {
-    console.log(org);
+    console.log(`org: ${org}`);
     octokit.rest.repos.listForOrg({ org: org }).then((data, headers, status) => {
+        console.log(`data: ${data}\n\n`);
         for (const repo in data) {
             githubLabelSync({
                 // Avoid broadly destructive actions - if allowAddedLabels is
@@ -21,7 +22,11 @@ for (const org of config['orgs']) {
                 repo: repo['full_name'],
             }).then((diff) => {
                 console.log(`\n\n Repository: ${repo['full_name']}:\n${diff}`)
+            }).catch((err) => {
+                console.log(err);
             });
         }
+    }).catch((err) => {
+        console.log(err);
     });
 }
