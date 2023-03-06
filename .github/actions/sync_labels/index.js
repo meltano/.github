67a8641d6b36b4af7ba601fa7df098436d0e8ead
Will Da Silva <will@willdasilva.xyz>
2023-03-06 10:37:54 -0500

    feat: Auto-generate common aliases (#9)

--- .github/actions/sync_labels/index.js
+++ .github/actions/sync_labels/index.js
@@ -13,6 +13,54 @@ const logAndExit = function (err) {
     process.exit(1);
 }
 
+String.prototype.toTitleCase = function() {
+    return this.replace(/(^|[\s\/\:\-])\S/g, (x) => { return x.toUpperCase(); });
+}
+
+// Auto-generates aliases from the label name and manually specified aliases.
+// Generates alernative aliases with the text:
+// - UPPERCASE
+// - lowercase
+// - Title Case
+// - With '::' repalced by '/'
+// - With '/' repalced by '::'
+// For example, a label named 'valuestream/Meltano' would receive the following aliases:
+// - 'VALUESTREAM/MELTANO'
+// - 'valuestream/meltano'
+// - 'Valuestream/Meltano'
+// - 'valuestream::Meltano'
+// - 'VALUESTREAM::MELTANO'
+// - 'valuestream::meltano'
+// - 'Valuestream::Meltano'
+// A label named 'wont fix' with the alias 'wontfix' would receive the following aliases:
+// - 'WONT FIX'
+// - 'Wont Fix'
+// - 'wontfix'
+// - 'WONTFIX'
+// - 'Wontfix'
+const generateAliases = function (config) {
+    for (const label of config['labels']) {
+        const aliases = new Set(
+            [label['name'], ...(label['aliases'] ?? [])].flatMap((text) => {
+                return [
+                    text,
+                    text.toUpperCase(),
+                    text.toLowerCase(),
+                    text.toTitleCase(),
+                ].flatMap((text) => {
+                    return [
+                        text.replace('::', '/'),
+                        text.replace('/', '::'),
+                    ];
+                });
+            })
+        );
+        aliases.delete(label['name']);
+        label['aliases'] = Array.from(aliases);
+    }
+    return config;
+}
+
 for (const org of config['orgs']) {
     octokit.rest.repos.listForOrg({ org: org }).then((data) => {
         for (const repo of data['data']) {
@@ -23,7 +71,7 @@ for (const org of config['orgs']) {
                 allowAddedLabels: true,
                 accessToken: core.getInput('token'),
                 dryRun: core.getBooleanInput('dry-run'),
-                labels: config['labels'],
+                labels: generateAliases(config['labels']),
                 repo: repo['full_name'],
             }).then((diff) => {
                 console.log(`\n\n ${repo['full_name']}:\n${JSON.stringify(diff, null, 4)}`)
